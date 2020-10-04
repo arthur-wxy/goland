ea0402cf2aae915d65aaf57bae46cba51cc8ef65
arthur_wxy <kzzn@qq.com>
2020-10-04 21:03:25 +0800

    use atom test

--- hello.go
+++ hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var attack = 40
+	var defence = 20
+	var damageRate float32 = 0.17
+	var damage = float32(attack - defence) * damageRate
 	fmt.Printf("hello golang\n")
+	fmt.Println(damage)
 }
