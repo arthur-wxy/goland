339aba97c90c13c67ebb5af5cd25f95aac575df6
arthur_wxy <kzzn@qq.com>
2020-10-23 10:47:50 +0800

    .

--- hello.go
+++ hello.go
@@ -3,10 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var attack = 40
-	var defence = 20
-	var damageRate float32 = 0.17
-	var damage = float32(attack - defence) * damageRate
-	fmt.Printf("hello golang\n")
-	fmt.Println(damage)
+	fmt.Printf("hello coder\n")
 }
